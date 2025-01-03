6ad9871bc60bfe9b51570992b8136aec3136bd1f
Hishmat786 <hishmatdrawarh786@gmail.com>
2025-01-03 15:01:45 +0500

    added usestate in form

--- Frontend/src/Components/Form.jsx
+++ Frontend/src/Components/Form.jsx
@@ -1,17 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./style.css"
 function Form() {
+
+    const [title, settitle] = useState("Fitness Champion")
+    const [discription, setDiscription] = useState("Complete 10 workouts.")
+    const [requirement, setRequirement] = useState("10")
 
     function handlesubmit(){
         alert("submit")
     }
+    
   return (
     <div className='form-container'>
         <h1>Add/Edit Award</h1>
         <form action="submit" onSubmit={handlesubmit} className='form'>
-        <input type="text" id="awardTitle" placeholder="Award Title" value="Fitness Champion"/>
-        <input type="text" id="awardDescription" placeholder="Award Description" value="Complete 10 workouts."/>
-        <input type="text" id="awardRequirement" placeholder="Requirements (e.g., 10)" value="10"/>
+        <input type="text" id="awardTitle" placeholder="Award Title" value={title}/>
+        <input type="text" id="awardDescription" placeholder="Award Description" value={discription}/>
+        <input type="text" id="awardRequirement" placeholder="Requirements (e.g., 10)" value={requirement}/>
         <button onSubmit={handlesubmit} >Submit</button>
         </form>
     </div>
