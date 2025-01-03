2d5f463eee71021e7c507d89139e9adc7d51215e
Hishmat786 <hishmatdrawarh786@gmail.com>
2025-01-03 14:52:16 +0500

    Add Card Component

---
+++ Frontend/src/Components/Card.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import "./Card.css"
+
+function Card() {
+  return (
+    <div className='card'>
+      <div className="award-card" id="award-1">
+        <span className="delete-icon">&times;</span>
+        <h3>Fitness Champion</h3>
+        <p>Complete 10 workouts.</p>
+        <div className="progress-bar">
+          {/* <div className="progress" style="width: 50%;"></div> */}
+        </div>
+        <button className="update-button">Update Progress</button>
+        <button className="update-button">Edit</button>
+      </div>
+    </div>
+  )
+}
+
+export default Card
--- Frontend/src/Components/Home.jsx
+++ Frontend/src/Components/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Form from './Form'
+import Card from './Card'
 
 function Home() {
   return (
     <div>
         {/* <h1>Home Page</h1> */}
         <Form/>
+        <Card/>
     </div>
   )
 }
